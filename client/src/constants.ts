056f59f2828ed100c9cb56d892d30c98c3fd76f5
Niraj Shaw <nirazshaw156@gmail.com>
2024-12-06 18:18:47 +0530

    added backend url

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -2,7 +2,7 @@ const COMPREHENSION = "comprehension";
 const CLOZE = "cloze";
 const CATEGORY = "category";
 const SERVER_DEV_URL = "http://localhost:4000";
-const SERVER_PROD_URL = null;
+const SERVER_PROD_URL = "https://super-assistant-assignment-d7bx.onrender.com";
 const SERVER_URL = SERVER_PROD_URL || SERVER_DEV_URL;
 
 export { COMPREHENSION, CATEGORY, CLOZE, SERVER_URL };
